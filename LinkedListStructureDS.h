6e182e275ff9ea38842f536dc6c22a692cfeeb00
Murilo Alves <hi@omurilo.dev>
2025-11-10 21:35:49 -0300

    feat: implement concrete data structure classes

---
+++ LinkedListStructureDS.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "AbstractDataStructure.h"
+#include "Node.h"
+
+class LinkedListStructure : public AbstractDataStructure {
+public:
+    LinkedListStructure();
+    ~LinkedListStructure();
+    void insert(size_t index, int value) override;
+    void remove(size_t index) override;
+    std::optional<int> access(size_t index) const override;
+private:
+    Node* head = nullptr;
+    void reconnectNodes();
+};
